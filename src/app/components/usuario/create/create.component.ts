74d17e8f6197e457ccb8aff50c9c188562d269cf
HDelsoto <higordel@hotmail.com>
2021-01-12 09:27:01 -0300

    Validacao Cpf

--- src/app/components/usuario/create/create.component.ts
+++ src/app/components/usuario/create/create.component.ts
@@ -27,7 +27,10 @@ export class CreateComponent implements OnInit {
     
   }
   createUsuario(): void{
-    this.uniqueCpf()
+    var scpf =new String();
+    scpf= this.splitCpf(this.usuario.cpf);
+    console.log(this.validaCpf(scpf));
+    this.usuario.cpf = scpf.toString();
     this.usuarioService.readByCpf(this.usuario.cpf).subscribe(usuario => {
       console.log("retorno unique", Object.keys(usuario).length) 
       if(Object.keys(usuario).length===0) {
@@ -49,14 +52,48 @@ export class CreateComponent implements OnInit {
          if(Object.keys(usuario).length===0) console.log("Cpf Unico")
          else console.log("Cpf Duplicado")
       })
-    }
-    
-      
-    
-  
+    } 
 
   cancel(): void{ 
     this.router.navigate(['/usuarios'])
+  }
+
+  splitCpf(cpf : any): String{
+    var splitted = cpf.split("-", 2);
+    if (splitted.length>1) var teste = splitted[0].toString().concat(splitted[1].toString());
+    else var teste = splitted[0].toString()
+    splitted = teste.split(".", 3)
+    if(splitted.length>1) teste =  splitted[0].toString().concat(splitted[1].toString(),splitted[2].toString());
+    else teste =  splitted[0].toString()
+    console.log (teste.toString());
+    return teste.toString();
+   
+  }
+
+  validaCpf(strCPF : String): Boolean{
+    var Soma;
+    var Resto;
+    var i;
+    Soma = 0;   
+    //strCPF  = RetiraCaracteresInvalidos(strCPF,11);
+    if (strCPF == "00000000000")
+	  return false;
+    for (i=1; i<=9; i++)
+	     Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i); 
+       Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11)) 
+	     Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) )
+	    return false;
+	Soma = 0;
+    for (i = 1; i <= 10; i++)
+       Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+       Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11)) 
+	     Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) )
+        return false;
+    return true;
   }
    
 
